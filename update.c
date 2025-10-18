c41f001fb59c0e5eda96b66da64f21ee86f01d22
aamnahalam <saamnahalam@gmail.com>
2025-10-18 18:19:08 +0500

    updating flights

---
+++ update.c
@@ -0,0 +1,19 @@
+void updateFlights() {
+int id, found = 0;
+printf("Enter Flight ID: \n");
+scanf("%d", &id);
+for(int i=0;i < count; i++)
+{
+  if(flights[i].id == id);
+{
+found = 1;
+printf("Enter new source: \n");
+scanf("%s", &flights[i].source);
+printf("Enter new Destination: \n");
+scanf("%s", %flights[i].destination);
+printf("Enter new seats: \n");
+scanf("%d",&flights[i].seats);
+printf("FLIGHT UPDATED SUCESSFULLY !\n");
+}
+}
+}
