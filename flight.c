c36fa37b945090c0bb226c7a496a1457b390a21b
aamnahalam <saamnahalam@gmail.com>
2025-10-18 15:25:25 +0500

    function definition

---
+++ flight.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#define MAX_FLIGHTS 100
+#define MAX_PASSENGERS 100
+#define MAX_BOOKINGS 100
+void addFlights();
+void updateFlights();
+void deleteFlights();
+void showallFlights();
+void addPassenger();
+void showAllPassengers();
+void bookTicket();
+void cancelTicket();
+void showAllBookings();
+void printMainMenu();
+void FlightMenu();
+void PassengerMenu();
+void BookingMenu();
+void printmenu();
+struct Flight
+struct Passenger
+struct Booking
