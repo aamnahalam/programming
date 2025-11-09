89f3ce6caabcf7204b7c1ccf815596d384252d5b
aamnahalam <saamnahalam@gmail.com>
2025-11-09 21:41:33 +0500

    Delete counters.c

--- counters.c
+++
@@ -1,9 +0,0 @@
-struct Flight flights[100];
-int flightCount = 0;
-
-struct Passenger passengers[100];
-int passengerCount = 0;
-
-struct Booking bookings[100];
-int bookingCount = 0;
-int nextBookingId = 1;
